e45ff9275f18c9e2b65c31216edeb62ef1e5199a
nabils24 <nabilsahsadacode@gmail.com>
2024-04-08 19:30:58 +0700

    update - ignoreBuildErrors

--- next.config.js
+++ next.config.js
@@ -25,6 +25,7 @@ const nextConfig = {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
+    ignoreBuildErrors: true,
   },
 };
 
